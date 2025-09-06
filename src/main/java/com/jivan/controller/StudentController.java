434ea6619c3dd97d583873a262919a825c15b788
HeavyCoderJivan <khose.jivan01@gmail.com>
2025-09-06 12:11:11 +0530

    added new pojo class with field and written api inside Studentcontroller

--- src/main/java/com/jivan/controller/StudentController.java
+++ src/main/java/com/jivan/controller/StudentController.java
@@ -1,5 +1,26 @@
 package com.jivan.controller;
+import java.util.ArrayList;
+import java.util.List;
 
-public class StudentController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jivan.pojo.Student;
+
+@RestController
+public class StudentController 
+{
+	private List<Student> stud=new ArrayList<>(List.of(
+			new Student(1, "Jivan", 22, "jivan@example.com", "Spring Boot"),
+			new Student(2, "Amit", 23, "amit@example.com", "ReactJS"),
+			new Student(3, "Ravi", 21, "ravi@example.com", "Python")));
+	
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudent()
+	{
+		return stud;
+		
+	}
 
 }
---
+++ src/main/java/com/jivan/pojo/Student.java
@@ -0,0 +1,18 @@
+package com.jivan.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student 
+{
+	private int id;
+	private String name;
+	private int age;
+	private String email;
+	private String course;
+
+}
